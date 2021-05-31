6defce44673254fcc31f74a0df33772a574ab631
Jonitos930 <85134115+Jonitos930@users.noreply.github.com>
2021-05-31 22:05:18 +0100

    Delete exe15.py

--- exe15.py
+++
@@ -1,5 +0,0 @@
-print("Olá, bom tudo bem ?")
-nome=str(input("Coloque aqui o seu nome:"))
-trabalho=int(input("Coloque aquI quantos dias de trabalho teve esse mes:"))
-calculo=(trabalho*25)*8
-print("O dinheiro que foi ganho:",calculo,"\nEsse é o seu salario,",nome)
