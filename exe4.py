22586c4666ab46218fdfa9eaf71419decf214103
Jonitos930 <85134115+Jonitos930@users.noreply.github.com>
2021-05-31 22:06:02 +0100

    Delete exe4.py

--- exe4.py
+++
@@ -1,6 +0,0 @@
-dg1 = int(input("Coloca ai um numero ai:")) 
-dg2 = int(input("Coloca outro numero ai:"))
-soma = dg1 + dg2 
-print("A soma entre",dg1,"e",dg2,"é igual a :",soma)
-x=float(input("Coloque aqui o angulo:"))
-print(math.sin(math.radians(x)))
